3dd4133a6c7aab522a240c7c547a7ce0020d126c
zhehongen <zhehongen@163.com>
2020-10-15 17:57:07 +0800

    删除测试

--- src/main/java/com/example/ftpdemo/ftp/MyDownloadFtplet.java
+++ src/main/java/com/example/ftpdemo/ftp/MyDownloadFtplet.java
@@ -5,6 +5,7 @@ import org.apache.ftpserver.ftplet.*;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 
 /**
  * @author xinfan.zhou@things-matrix.com
@@ -45,6 +46,37 @@ public class MyDownloadFtplet extends DefaultFtplet {
         log.info("absolutePath:  " + absolutePath);
         log.info("状态：成功~，用户:'{}'，从目录下载文件：'{}'，文件名称为：'{}'", name, path + absolutePath, filename);
         return super.onDownloadEnd(session, request);
+    }
+
+
+    @Override
+    public FtpletResult onConnect(FtpSession session) throws FtpException,
+            IOException {
+        InetSocketAddress clientAddress = session.getClientAddress();
+        //获取用户
+        User name = session.getUser();
+        String userArgument = session.getUserArgument();
+        InetSocketAddress serverAddress = session.getServerAddress();
+        log.info(">>>>>>>>>>>>>>onConnect serverAddress: {}", serverAddress.toString());
+        log.info(">>>>>>>>>>>>>>onConnect userArgument:{}", userArgument);
+        log.info(">>>>>>>>>>>>>>onConnect user:{}", name);
+        log.info(">>>>>>>>>>>>>>onConnect client address: {}", clientAddress.toString());
+        return super.onConnect(session);
+    }
+
+    @Override
+    public FtpletResult onDisconnect(FtpSession session) throws FtpException,
+            IOException {
+        InetSocketAddress clientAddress = session.getClientAddress();
+        //获取用户
+        User name = session.getUser();
+        String userArgument = session.getUserArgument();
+        InetSocketAddress serverAddress = session.getServerAddress();
+        log.info(">>>>>>>>>>>>>>onDisconnect serverAddress: {}", serverAddress.toString());
+        log.info(">>>>>>>>>>>>>>onDisconnect userArgument:{}", userArgument);
+        log.info(">>>>>>>>>>>>>>onDisconnect user:{}", name);
+        log.info(">>>>>>>>>>>>>>onDisconnect client address: {}", clientAddress.toString());
+        return super.onDisconnect(session);
     }
 
 }
